eb155eeacc4b2e98a496da9e93c10981c402456e
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-10-31 09:14:39 +0700

    Add customer, order item, order model.

---
+++ app/models/customer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Customer < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_fullname, against: %i[first_name last_name], using: {
+    tsearch: { prefix: true }
+  }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+
+  def initialize(*args)
+    super(*args)
+
+  end
+end
