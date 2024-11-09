259d11b6afffb8f4b6019d4b2d228ecb002dfb07
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-11-09 11:29:02 +0700

    Add get order API.

--- app/api/v1/users/orders.rb
+++ app/api/v1/users/orders.rb
@@ -4,6 +4,27 @@ module V1
   module Users
     class Orders < Base
       resources :orders do
+
+        desc 'Get all orders',
+              summary: 'Get all orders'
+        params do
+          optional :customer_id, type: Integer, desc: 'Search order by customer ID'
+          optional :max_total_amount, type: Integer, desc: 'Search order by max total price'
+          optional :min_total_amount, type: Integer, desc: 'Search order by min total price'
+          optional :page, type: Integer, desc: 'Page number'
+          optional :per_page, type: Integer, desc: 'Per page number'
+        end
+        get do
+          orders = Order.all
+          orders = orders.where(customer_id: params[:customer_id]) if params[:customer_id].present?
+          orders = orders.where('total_amount <= ?', params[:max_total_amount]) if params[:max_total_amount].present?
+          orders = orders.where('total_amount >= ?', params[:min_total_amount]) if params[:min_total_amount].present?
+          
+          paginated_response(orders)
+
+          rescue StandardError => e
+            error!(e.message, 400)
+        end
         desc 'Create a new order',
              summary: 'Create a new order'
         params do
@@ -18,8 +39,8 @@ module V1
           format_response(order)
           rescue StandardError => e
             error!(e.message, 400)
-          end
         end
+      end
     end
   end
 end
