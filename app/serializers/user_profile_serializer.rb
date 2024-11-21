4c2dbe54bd6df986531129102fca0167d5edc262
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-11-21 22:41:38 +0700

    Add role to user profile serializer.

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserProfileSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :first_name, :last_name, :created_at, :updated_at
+  attributes :id, :username, :email, :first_name, :role, :last_name, :created_at, :updated_at
 end
