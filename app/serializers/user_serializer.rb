36e0ebaecd1362e8c22f75c615f869b6fdd96484
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-11-22 11:11:07 +0700

    Add points to serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :birth, :email, :role, :first_name, :last_name, :created_at, :updated_at
+  attributes :id, :username, :birth, :email, :role, :first_name, :last_name, :points, :created_at, :updated_at
 end
