9d318ba9eed060ddebb18dd4f94433d2ecb8538d
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-03 14:44:15 +0700

    Update product batch serializer.

--- app/serializers/product_batch_serializer.rb
+++ app/serializers/product_batch_serializer.rb
@@ -1,3 +1,3 @@
 class ProductBatchSerializer < ActiveModel::Serializer
-  attributes :id, :batch_number, :quantity, :price, :expiration_date, :manufacture_date
+  attributes :id, :batch_number, :quantity, :stock, :import_price, :price, :expiration_date, :manufacture_date
 end
